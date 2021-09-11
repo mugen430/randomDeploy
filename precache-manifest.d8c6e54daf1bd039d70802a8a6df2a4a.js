7c8cbfe148588ee7b7335ce7d851beb77d4f8a36
Maverick Acampado <acampado.maverick@gmail.com>
2021-09-10 20:39:39 -0500

    testing

---
+++ precache-manifest.d8c6e54daf1bd039d70802a8a6df2a4a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11815dc3838a6db456ffe1868fcd4d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9ad403fde81ac7e949a",
+    "url": "/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "d4c5d8acebe39d0d4588",
+    "url": "/static/css/main.6d64b71b.chunk.css"
+  },
+  {
+    "revision": "a9ad403fde81ac7e949a",
+    "url": "/static/js/2.de9db4d3.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.de9db4d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4c5d8acebe39d0d4588",
+    "url": "/static/js/main.1401d6da.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "efdb999a04a21ac31316e4a0830d6737",
+    "url": "/static/media/img1.efdb999a.png"
+  }
+]);
